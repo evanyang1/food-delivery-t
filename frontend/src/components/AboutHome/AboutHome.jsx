8afb060849cc28dee6882a2c4d3d5a0c221e1f4c
Evan Yang <evanjyang4@gmail.com>
2025-07-28 08:33:58 -0700

    fix animation + start our home menu

--- frontend/src/components/AboutHome/AboutHome.jsx
+++ frontend/src/components/AboutHome/AboutHome.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { aboutfeature } from "../../assets/dummydata";
+import { FaInfoCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import AboutImage from "../../assets/AboutImage.png";
+import FloatingParticle from "../FloatingParticle/FloatingParticle";
+import "./AboutHome.css";
 
 const AboutHome = () => {
   return (
@@ -49,21 +54,71 @@ const AboutHome = () => {
 
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-8 px-4 sm:px-0">
             {aboutfeature.map((item, i) => (
-                <div key={i} className="flex flex-col items-center justify-center gap-3 sm:gap-4 transition-transform
-                duration-300 p-4 sm:p-5 hover:translate-x-2">
-                    <div className={`p-3 sm:p-4 rounded-full bg-gradient-to-r ${item.color} 
-                    transition-transform duration-300 group-hover:scale-110`}>
-                        <item.icon className="text-2xl sm:text-3xl text-white" />
-                    </div>
-                    <div className="text-center">
-                        <h3 className="text-xl sm:text-2xl font-bold font-cursive">{item.title}</h3>
-                        <p className="opacity-80 text-sm sm:text-base">{item.text}</p>
-                    </div>
+              <div
+                key={i}
+                className="flex flex-col items-center justify-center gap-3 sm:gap-4 transition-transform
+                duration-300 p-4 sm:p-5 hover:translate-x-2"
+              >
+                <div
+                  className={`p-3 sm:p-4 rounded-full bg-gradient-to-r ${item.color} 
+                    transition-transform duration-300 group-hover:scale-110`}
+                >
+                  <item.icon className="text-2xl sm:text-3xl text-white" />
                 </div>
+                <div className="text-center">
+                  <h3 className="text-xl sm:text-2xl font-bold font-cursive">
+                    {item.title}
+                  </h3>
+                  <p className="opacity-80 text-sm sm:text-base">{item.text}</p>
+                </div>
+              </div>
             ))}
           </div>
+
+          <div className="flex flex-wrap gap-4 items-center mt-6 sm:mt-8 px-4 sm:px-0">
+            <Link
+              to="/about"
+              className="px-4 sm:px-6 py-2 sm:py-3 bg-gradient-to-r from-amber-500 to-orange-600
+            rounded-xl font-bold hover:scale-[1.02] transition-transform duration-300 flex items-center gap-2 sm:gap-3
+            shadow-xl hover:shadow-amber-500/20 group relative overflow-hidden "
+            >
+              <span
+                className="absolute inset-0 bg-gradient-to-r from-amber-600/30 to-transparent
+              opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+              ></span>
+              <FaInfoCircle className="text-lg sm:text-xl animate-pulse" />
+              <span className="font-cursive text-lg sm:text-xl">
+                Unveil our Legacy
+              </span>
+            </Link>
+          </div>
+        </div>
+
+        <div
+          className="w-full order-2 lg:order-1 md:max-w-md lg:max-w-none lg:w-7/12 mt-12 mb-10 relative
+        group transform hover:scale-[1.01] transition-all duration-500"
+        >
+          <div
+            className="relative rounded-[4rem] overflow-hidden border-4 border-amber-900/30 
+          hover:border-amber-600/40 transition duration-500 shadow-2xl shadow-black/50"
+          >
+            <div className="absolute inset-0 bg-gradient-to-r from-amber-400/15 via-transparent to-amber-600/10 mix-blend-light" />
+            <img
+              src={AboutImage}
+              alt="Restaurant"
+              className="w-full h-auto object-cover aspect-[3/4] transform
+              -rotate-1 hover:rotate-0 transition-all duration-500"
+            />
+            <div
+              className="absolute -bottom-12 left-1/2 -translate-x-1/2 w-4/5 h-16 bg-amber-900/30 blur-3xl
+            z-0"
+            />
+          </div>
+
+          <div className="absolute -top-6 -right-6 w-24 h-24 bg-amber-500/10 rounded-full blur-xl" />
         </div>
       </div>
+      <FloatingParticle />
     </div>
   );
 };
---
+++ frontend/src/components/OurHomeMenu/OurHomeMenu.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const OurHomeMenu = () => {
+  return (
+    <div
+      className="bg-gradient-to-br from-[#1a120b] via-[#2a1e14] to-[#3e2b1d] min-h-screen py-16
+    px-4 sm:px-6 lg:px-8"
+    >
+      <div className="mx-w-7xl mx-auto">
+        <h2
+          className="text-4xl sm:text-5xl md:text-6xl font-bold text-center mb-12 bg-clip-text text-transparent
+        bg-gradient-to-r from-amber-200 via-amber-300 to-amber-200"
+        >
+          <span className="font-dancing-script block text-5xl md:text-7xl sm:text-6xl mb-2">
+            Our Exquisite Menu
+          </span>
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default OurHomeMenu;
