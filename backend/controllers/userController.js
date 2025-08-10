8fe8b477cb3e599771068aef1dff2ae88a5007d8
Evan Yang <evanjyang4@gmail.com>
2025-08-10 09:13:02 -0700

    user routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const userModel = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const validator = require("validator");
 
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
@@ -24,5 +25,42 @@ const loginUser = async (req, res) => {
 };
 
 const createToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET)
-}
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
+
+const registerUser = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const userExists = await userModel.findOne({ email });
+    if (userExists) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
+    }
+
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+    if (password.length < 8) {
+      return res.status(400).json({ message: "Password must be at least 8 characters long" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = await userModel({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    const user = await newUser.save();
+    const token = createToken(user._id);
+    res.status(201).json({ success: true, token });
+
+  } catch (error) {
+    console.error("Registration error:", error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { loginUser, registerUser };
--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,25 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const connectDB = require("./config/db.js");
+const userRouter = require("./routes/userRoute");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS policy violation"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -15,6 +28,8 @@ app.use(express.urlencoded({ extended: true }));
 connectDB();
 
 // routes
+app.use("/api/user", userRouter);
+
 app.get("/", (req, res) => {
   res.send("Welcome to the Food Delivery API!");
 });
